d8e6904ea94ab68451958f95a6b15ae1446eb40f
Lucas X <lucasmxisto@outlook.com>
2022-10-16 00:46:56 -0300

    started the calculate total price function

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(){
-    updateTotalPrice();
-    const quantitiesInput = document.getElementById('quant');
-    quantitiesInput.addEventListener('change', updateTotalPrice);
+document.addEventListener('DOMContentLoaded',function(){
+    const quantityProd = document.getElementsByClassName('quantity');
+    quantityProd.addEventListener('change', calcPrice());
 })
+function calcPrice(){
+    const quantityProd = document.getElementsByClassName('quantity');
+    const unitPrice = document.getElementsByClassName('unit-price');
+    const totalPrice = document.getElementsByClassName('total-price');
+    for (let i = 0; i < unitPrice.length; i++){
+        totalPrice[i].innerHTML = Number.parseFloat(unitPrice[i].innerHTML) * quantityProd[i].value;
+    }
 
-function updatePrices(){
-    const quantitiesInput = document.getElementById('quant');
-    quantitiesInput.addEventListener('change', updateTotalPrice);
-}
-function updateTotalPrice(){
-    const quantitiesInput = document.getElementById('quant');
-    const unitPriceProduct = document.getElementById('unitPrice');
-    const totalPriceProduct = document.getElementById('totalPrice');
-    totalPriceProduct.innerHTML = Number.parseFloat(quantitiesInput.value) * Number.parseFloat(unitPriceProduct.value);
 }
