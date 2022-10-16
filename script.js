0f82f6c2213fa04d2d1a6aefb947327d95efa806
Lucas X <lucasmxisto@outlook.com>
2022-10-16 01:10:23 -0300

    finished the calculate prices function

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ function calcPrice(event){
     const quantityProd = document.getElementsByClassName('quantity');
     const unitPrice = document.getElementsByClassName('unit-price');
     const totalPrice = document.getElementsByClassName('total-price');
+    let totalCart = document.getElementById('cart-total');
+    controlNumber = 0;
     for (let i = 0; i < unitPrice.length; i++){
         totalPrice[i].innerHTML = Number.parseFloat(unitPrice[i].innerHTML) * quantityProd[i].value;
+        controlNumber += Number.parseFloat(totalPrice[i].innerHTML);
     }
-
+    totalCart.innerHTML = controlNumber;
 }
